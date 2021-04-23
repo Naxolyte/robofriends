080c2603bc213d5251837198ed8f5a92b5225d05
Naxolye <dani.emrah@gmail.com>
2021-04-23 16:32:36 +0300

    json users changes

--- src/containers/App.js
+++ src/containers/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users', { mode: 'no-cors'})
+    fetch('https://jsonplaceholder.cypress.io/users')
       .then((response) => response.json())
       .then((users) => this.setState({ robots: users }));
   }
